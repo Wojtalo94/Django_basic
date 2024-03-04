f4606a9f71a41e9ceb852c4e7c71628148290577
wojtalo94 <wojtek94@interia.eu>
2024-03-04 18:31:05 +0100

    ID-01 - new 2 tables

--- mysite/ankiety/models.py
+++ mysite/ankiety/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Każda tabela ma kilka zmiennych, każda zmiennych musi być reprezentowana przez typ danych, poniżej użwamy właśnie 'Field'. 'CharField' jest dla pól znakowych, z wartością maksymalną znaków ustawioną na 200, co jest obowiązkowe!. 'DateTimeField' oznacza datę publikacji. Pole 'IntegerField' jest to pole liczbowe, a wartość domyślna 'default' jest opcjonalna. 'ForeignKey' dodatkowy, obcy klucz za pomocą którego łączymy tabelę Choice z Question. 
+class Question(models.Model):
+    # czyli mamy tabelę Questions z polami question_text i pub_date
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    # czyli mamy tabelę Choice z polami question, choice_text i votes
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
